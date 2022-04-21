b7867c44b79d24d523fe564e7f32447a3db218ed
lainbo <46062895+lainbo@users.noreply.github.com>
2022-04-21 21:47:59 +0800

    pref: 优化自动暗色模式

--- src/utils/setTheme.js
+++ src/utils/setTheme.js
@@ -1,22 +1,18 @@
+const bodyDom = document.body // body的dom
 const isDark = useDark() // 响应式：是否为暗色
-const htmlDom = document.documentElement // html的dom
-// 切换为深色
-const setThemeDark = () => {
-  htmlDom.classList.remove('light')
-  htmlDom.classList.add('dark')
-  document.body.setAttribute('arco-theme', 'dark')
+
+/**
+ * 设置主题
+ * @param {Boolean} val true: 深色，false: 浅色
+ */
+export function setTheme(val) {
+  document.documentElement.classList.toggle('dark', val)
+  val
+    ? bodyDom.setAttribute('arco-theme', 'dark')
+    : bodyDom.removeAttribute('arco-theme')
 }
 
-// 切换为浅色
-const setThemeLight = () => {
-  htmlDom.classList.remove('dark')
-  htmlDom.classList.add('light')
-  document.body.removeAttribute('arco-theme')
-}
 // 监听是否暗色
-watch(isDark, () => setTheme())
-
-// 主题初始化
-const setTheme = () => {
-  isDark.value ? setThemeDark() : setThemeLight()
-}
+watchEffect(() => {
+  setTheme(isDark.value)
+})
