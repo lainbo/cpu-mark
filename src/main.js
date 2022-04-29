955ad60f6632fb33a14d33452cba237ace41ed21
liangjiacheng <lainbo@foxmail.com>
2022-04-29 11:22:21 +0800

    fix: 修复排序功能消失的问题

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import 'virtual:windi.css'
 import '@/styles/global.scss'
 import '@arco-design/web-vue/es/message/style/css.js'
 import 'xe-utils'
-import { Header, Column, Tooltip, Table } from 'vxe-table'
+import { Header, Icon, Column, Tooltip, Table } from 'vxe-table'
 
 function useTable(app) {
-  app.use(Header).use(Column).use(Tooltip).use(Table)
+  app.use(Header).use(Icon).use(Column).use(Tooltip).use(Table)
 }
 
 createApp(App).use(useTable).mount('#app')
