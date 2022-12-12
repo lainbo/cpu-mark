2ee62c4f96f3ae2d3a5cfb94c9635353fd6ecbb8
lainbo <lainbo@foxmail.com>
2022-12-12 12:15:52 +0800

    update: 更新依赖，更新数据

--- src/App.vue
+++ src/App.vue
@@ -57,26 +57,26 @@ const hardDriveData = uniqArr(hardDriveOriginData)
 
 const activeName = ref(1) // 默认选中的tab
 const updateObj = reactive({
-  date: '2022年11月27日',
+  date: '2022年12月12日',
   updateArr: [
     {
       label: 'CPU多核',
-      addNum: 7,
+      addNum: 10,
       totalNum: cpuMData.length,
     },
     {
       label: 'CPU单核',
-      addNum: 2,
+      addNum: 10,
       totalNum: cpuSData.length,
     },
     {
       label: '显卡',
-      addNum: 1,
+      addNum: 4,
       totalNum: gpuData.length,
     },
     {
       label: '硬盘',
-      addNum: 26,
+      addNum: 11,
       totalNum: hardDriveData.length,
     },
   ],
